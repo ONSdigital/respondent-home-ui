11b0fb1ecaf03260d31daa49b4cd8a98abb8d19b
John Topley <john.topley@ons.gov.uk>
2016-08-09 10:26:27 +0100

    CTPA-591 Using the ONS JWE gem for eQ token generation

--- routes/home.rb
+++ routes/home.rb
@@ -3,10 +3,12 @@ require 'sinatra/content_for2'
 require 'sinatra/flash'
 require 'sinatra/formkeeper'
 require 'rest_client'
+require 'ons-jwe'
+require 'openssl'
 require 'json'
 require 'yaml'
-require 'openssl'
 
+KEY_ID                    = 'EDCRRM'
 SESSION_EXPIRATION_PERIOD = 60 * 60 * 6
 
 # Load various settings from a configuration file.
@@ -27,6 +29,24 @@ use Rack::Session::Cookie, key: 'rack.session', path: '/',
 # View helper for defining blocks inside views for rendering in templates.
 helpers Sinatra::ContentFor2
 helpers do
+  def claims(iac)
+    {
+      user_id: iac,
+      iat: Time.now.to_i,
+      exp: Time.now.to_i + 60 * 60,
+      eq_id: '1',
+      period_str: '2016-01-01',
+      period_id: '2016-01-01',
+      form_type: '0205',
+      collection_exercise_sid: '789',
+      ref_p_start_date: '2016-01-01',
+      ref_p_end_date: '2016-09-01',
+      ru_ref: '12346789012A',
+      ru_name: 'Office for National Statistics',
+      return_by: '2016-04-30',
+      employment_date: '2016-06-10'
+    }
+  end
 
   # View helper for escaping HTML output.
   def h(text)
@@ -60,6 +80,7 @@ post '/' do
     iac_response = []
     RestClient.get("http://#{settings.case_service_host}:#{settings.case_service_port}/questionnaires/iac/#{iac}") do |response, _request, _result, &_block|
       iac_response = JSON.parse(response)
+
       if response.code == 404
         flash[:notice] = 'Invalid Internet access code.'
         redirect '/'
@@ -67,50 +88,12 @@ post '/' do
         flash[:notice] = 'Questionnaire has been completed.'
         redirect '/'
       else
+        public_key  = load_key_from_file(settings.public_key)
+        private_key = load_key_from_file(settings.private_key,
+                                         settings.private_key_passphrase)
 
-        iat = Time.now.to_i
-        exp = Time.now.to_i + (60 * 60)
-
-        payload = {  user_id: iac_response['iac'],
-                     ru_ref: '789789789c',
-                     ru_name: 'APPLE',
-                     eq_id: '10',
-                     collection_exercise_sid: '789',
-                     period_id: '201605',
-                     period_str: 'May 2016',
-                     ref_p_start_date: '2016-07-01',
-                     ref_p_end_date: '2016-07-31',
-                     employment_date: '2016-06-10',
-                     trad_as: 'Apple',
-                     form_type: '2011',
-                     return_by: '2016-07-30',
-                     iat: iat,
-                     exp: exp
-                  }
-
-        priv_key = "-----BEGIN RSA PRIVATE KEY-----\nProc-Type: 4,ENCRYPTED\nDEK-Info: AES-256-CBC,C2C5740E6DA1DD915CA90CFFCBDFA5B2\n\nnlAOzfOUcrksZS2H0WLE27ME/wpuDo8rimRBxA++EHe+2HJsXyiNz+PRPHPOIvcW\nlGtPgLcfwT9A+pwico0s1u0N8/XQC21640r30xBQSW6Y+msPLoibnvl6bkDO0z/z\nx+mqCQvPTQEpJwwbtE8wFbrs74P0y6EUFIu8m8yPBg9Fmt8noAuR227l5tg/TLga\nHMFYT5mFO7g+oJvtJhOxXcCNf3EcoRQOlap9PHq2daVSTXy+qCTVe5AVDIlufidp\nfl4qGAqu5z2JbI8dsm+WJGtvqGacNoLOHsKHFGx/Qgy5em/NPAf9ak5fgjxQrrK1\nUWh+EymVsSQKN8MXPWYSzthSdoYAUuWu1iaDbcJw8CWU2vfmgXMxjTPUvsx0wRUX\n4tUfNtgqxcIsh1bH3MtcCeqxtyUOG96f2TGCx6gofUiKxwMoBzk3HqdgwexvZW1L\n64xgbY02AvztjxD8KFNWXQgji9mKorplqCIugpj67oRIwVOwdzfebiLk5eFb31Jj\nRZumElCCo2UtQe/fAqebBAC2CtaMuSBTozmHNPx9DDEGVQH6i/ZZHTHCvFG1+MJS\nPyTRQZnS4qnj+b7ObBNvAhSPsUD6hKI0yV3k1I3P+Hi1syMMIFKUwahzUReLF5le\nCdT4rMgrpHDyw5QUH5IjHzc0AXxoomM5UaUkk65E+MHU3nq37E7ExE6AFTdqmP4t\nI67Ed+bHNe6qxweDtuuc84Ok24k8OxRoZchrhFytWpk1lkijGUb/fRdmS4/6HiKJ\nRgkZ+VHi/FQN3/fedzfHouWYKaGKI51MEQfB7cSL8g13MchhGVOmu9TG6+F1/qRc\ndNil/8PE/cafxFEImW1v//ycLx0k/njfXyi/U2Vk8+WEDEtPe1fsF0F+vdnRH+l3\nzWsPtgqRqVAWZxn28xN8xNA9+iaoKH869/OVRDbmeU1tNQaSnCE3Ltkrg5Qoc8r5\nnPEviH812mnKkS299NVQFHbBSN2WDrf27gYKDn0ajSBo9GBeQJQOPTGWkMlHnOFB\nALphV1Ul2APJih7rgcHh6RubgN6X3EfFCL5VzSlpqGnYp9Ov1+U0iLjyfMWgLOd8\npXyIOl9qMJUZchjLsFXCj2u/SGuAu2ojMnG+0UFxYnx+f9l6UcUvEAYJIJop6XS9\ntB5rgsqQwYAB3N7yQ/C12fVc6qITffRxqBaGEe9yPFZO5XmUgn/oNtJuArSPBAIe\nInZI1TWbvAeVYfDQEJDsM1ND0X17MAPghQXH4nVavJydGbUY5sg9QJF2BBP6sT9Y\nzvgYUD5RZfzwuZOfdy46fv3VmqR2fYKuj1zVXFqGiINFsQb6airDt/11NJ0p8avo\n9X/eR20/uU6gtk5ABF2U3zepa3NheMhZJUmHysmYKiYwO6sJ1XTpAjNhmmLcOsEI\nXXCtQwd8uNYVzxSlbCSB3EQ841Ix9Z1+z/qUsFM01dX882It7qP/M7AdhogEYaPR\nZNMk0igVzh9T+6ErSOngoug0nLRqZsKlMhC7RNskMpIqt8iGLGnjLRT+4adwvCUW\nERd/A2RoDdedhgV49v9mYUWtENxGOr1VmOG98FzdmfUqwxNpukfhF9fz7iCB+Bnk\n-----END RSA PRIVATE KEY-----"
-        private_key  = OpenSSL::PKey::RSA.new(priv_key, 'digitaleq')
-        pub_key = "-----BEGIN PUBLIC KEY-----\nMIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAvZzMraB96Wd1zfHS3vW3\n0pi6IOu92MlIwdH/L6CTuzYnG4PACKT8FZonLw0NYBqh8p4vWS8xtNHNjTWua/FF\nz//Nkqz+9HfwViNje2Y5L6m3K/7raA0kUsWD1f6X7/LIJfkCEctCEj9q19+cX30h\nTlxdtYnEb9HbUZkg7dXAtnikozlE/ZZSponq7K00h3Uh9goxQIavcK1QI8pw5V+T\n8V8Ue7k98W8LpbYQWm7FPOZayu1EoJWUZefdOlYAdeVbDS4tjrVF+3za+VX3q73z\nJEfyLEM0zKrkQQ796gfYpkzDYwJvkiW7fb2Yh1teNHpFR5tozzMwUxkREl/TQ4U1\nkwIDAQAB\n-----END PUBLIC KEY-----"
-        public_key = OpenSSL::PKey::RSA.new(pub_key)
-
-        cek =  Random.new.bytes(32)
-        iv  =  Random.new.bytes(12)
-
-
-        jwt = JSON::JWT.new(payload)
-        jwt.kid = 'EDCRRM'
-        jwt.alg = :RS256
-
-
-        jws = jwt.sign(private_key, :RS256)
-
-
-        # encrypt the token
-        eqtoken = jws.encrypt(public_key,'RSA-OAEP', :A256GCM).to_s
-
-        logger.info eqtoken
-        url = "http://#{settings.eq_service_host}:#{settings.eq_service_port}/session?token=" + eqtoken
-        redirect url
+        token = JWEToken.new(KEY_ID, claims(iac), public_key, private_key)
+        redirect "http://#{settings.eq_service_host}:#{settings.eq_service_port}/session?token=#{token.value}"
       end
     end
   end

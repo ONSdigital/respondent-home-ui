0960a9be943038dd3ba7fc22c0094afce267bce3
John Topley <john.topley@ons.gov.uk>
2016-10-24 19:02:12 +0100

    CTPA-848 Added shell script for setting required environment variables in development

---
+++ env.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+#
+# Script for setting the required environment variables in development.
+# Adjust as required.
+#
+# Usage: source ./env.sh
+#
+# Author: John Topley (john.topley@ons.gov.uk)
+#
+export RESPONDENT_HOME_LOCALE="en"
+export RESPONDENT_HOME_EQ_HOST="eq-server"
+export RESPONDENT_HOME_EQ_PORT="5000"
+export RESPONDENT_HOME_IAC_SERVICE_HOST="localhost"
+export RESPONDENT_HOME_IAC_SERVICE_PORT="8121"

84d83e0567693cfdbf73f1bc2d8c567621a8cb9c
John Topley <john.topley@ons.gov.uk>
2016-11-02 14:18:55 +0000

    CTPA-858 Better Redis key names

--- lib/authentication_attempt_limiter.rb
+++ lib/authentication_attempt_limiter.rb
@@ -3,22 +3,24 @@ require 'redis'
 # Class to manage the number of IAC authentication attempts. Uses Redis as a
 # backing store.
 class AuthenticationAttemptLimiter
+  KEY_PREFIX = 'respondent.home:auth.attempt:'
+
   def initialize(redis_host, redis_port, max_attempts, ip_address)
     @redis_host   = redis_host
     @redis_port   = redis_port
     @max_attempts = max_attempts
-    @ip_address   = ip_address
-    attempts = attempt_counter_cache.get(@ip_address).to_i || 0
-    attempt_counter_cache.set(@ip_address, attempts) if attempts.zero?
+    @key          = "#{KEY_PREFIX}#{ip_address}"
+    attempts = attempt_counter_cache.get(@key).to_i || 0
+    attempt_counter_cache.set(@key, attempts) if attempts.zero?
   end
 
   def attempt!
-    attempts = attempt_counter_cache.get(@ip_address).to_i + 1
-    attempt_counter_cache.set(@ip_address, attempts)
+    attempts = attempt_counter_cache.get(@key).to_i + 1
+    attempt_counter_cache.set(@key, attempts)
   end
 
   def max_attempts?
-    attempt_counter_cache.get(@ip_address).to_i >= @max_attempts.to_i - 1
+    attempt_counter_cache.get(@key).to_i >= @max_attempts.to_i - 1
   end
 
   private

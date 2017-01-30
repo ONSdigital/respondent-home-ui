12bdc1d0fed16c3e803d0f4bea61e68a30c1ed38
John Topley <john.topley@ons.gov.uk>
2017-01-30 11:45:46 +0000

    Tidy up

---
+++ env.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+#
+# Script for setting the required environment variables in development.
+# Adjust as required.
+#
+# Usage: source ./env.sh
+#
+# Author: John Topley (john.topley@ons.gov.uk)
+#
+export RESPONDENT_HOME_ANALYTICS_ACCOUNT="UA-56892037-7"
+export RESPONDENT_HOME_EQ_HOST="eq-server"
+export RESPONDENT_HOME_EQ_PORT="5000"
+export RESPONDENT_HOME_EQ_PROTOCOL="http"
+export RESPONDENT_HOME_IAC_SERVICE_HOST="localhost"
+export RESPONDENT_HOME_IAC_SERVICE_PORT="8121"
+export RESPONDENT_HOME_IAC_SERVICE_PROTOCOL="http"
+export RESPONDENT_HOME_IAC_SERVICE_USER="admin"
+export RESPONDENT_HOME_IAC_SERVICE_PASSWORD="secret"

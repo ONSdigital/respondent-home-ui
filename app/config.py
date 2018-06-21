ba2276f01ede113c55e2e0090309e4f6558de5b2
James Gardiner <james@jgardiner.co.uk>
2018-06-21 11:20:49 +0100

    Update config with eq and iac required env vars

--- app/config.py
+++ app/config.py
@@ -1,5 +1,6 @@
 from envparse import env
 
+
 class Config(dict):
 
     def from_object(self, obj):
@@ -20,8 +21,20 @@ class Config(dict):
 class BaseConfig:
     HOST = env.str("HOST", default=None)
     PORT = env.int("PORT", default=None)
+    EQ_URL = env.str("EQ_URL", default=None)
+    IAC_URL = env.str("IAC_URL", default=None)
+    IAC_AUTH = (
+        env.str("IAC_USERNAME", default=None),
+        env.str("IAC_PASSWORD", default=None),
+    )
 
 
 class DevelopmentConfig:
     HOST = env.str("HOST", default="0.0.0.0")
     PORT = env.int("PORT", default="9092")
+    EQ_URL = env.str("EQ_URL", default='https://localhost:5000/session?token=')
+    IAC_URL = env.str("IAC_URL", default='http://localhost:8121')
+    IAC_AUTH = (
+        env.str("IAC_USERNAME", default="admin"),
+        env.str("IAC_PASSWORD", default="secret"),
+    )

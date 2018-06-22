fa8c073c697b1e10949199602597aade76d9a8ef
James Gardiner <james@jgardiner.co.uk>
2018-06-22 14:46:21 +0100

    Add session and flash message

---
+++ app/flash.py
@@ -0,0 +1,40 @@
+from copy import deepcopy
+from functools import partial
+
+from aiohttp import web
+from aiohttp_session import get_session
+
+SESSION_KEY = REQUEST_KEY = "flash"
+
+
+def flash(request, message):
+    request[REQUEST_KEY].append(message)
+
+
+def pop_flash(request):
+    flash = request[REQUEST_KEY]
+    request[REQUEST_KEY] = []
+    return flash
+
+
+@web.middleware
+async def flash_middleware(request, handler):
+    session = await get_session(request)
+    flash_incoming = session.get(SESSION_KEY, [])
+    request[REQUEST_KEY] = deepcopy(flash_incoming)  # copy flash for modification
+    try:
+        response = await handler(request)
+    finally:
+        flash_outgoing = request[REQUEST_KEY]
+        if flash_outgoing != flash_incoming:
+            if flash_outgoing:
+                session[SESSION_KEY] = flash_outgoing
+            else:
+                del session[SESSION_KEY]
+    return response
+
+
+async def context_processor(request):
+    return {
+        'get_flashed_messages': partial(pop_flash, request),
+    }
--- app/handlers.py
+++ app/handlers.py
@@ -1,14 +1,22 @@
+import time
+
 import aiohttp_jinja2
 from aiohttp.web import Response
+from aiohttp_session import get_session
 
 
 async def get_index(request):
-    context = {"a_variable": 12}
+    context = {}
+    session = await get_session(request)
+    session['flash'] = {"message": "hello!"}
+    session.changed()
     response = aiohttp_jinja2.render_template("index.html", request, context)
     return response
 
 
 async def post_index(request):
+    for message in pop_flash(request):
+        print(message)
     return Response(text="A post!")
 
 

e49fe4c3c4ba4c99df9af1dbad5d1a5a75f1d5b4
liamtoozer <liam.toozer@gmail.com>
2018-06-25 11:32:22 +0100

    Add handler for startup shutdown

--- app/handlers.py
+++ app/handlers.py
@@ -1,10 +1,11 @@
 import time
+from uuid import UUID
 
 import aiohttp_jinja2
 from aiohttp.web import Response
 from aiohttp_session import get_session
 
-from .flash import pop_flash
+from .flash import pop_flash, flash
 
 
 async def get_index(request):
@@ -13,7 +14,19 @@ async def get_index(request):
 
 
 async def post_index(request):
-    return Response(text="A post!")
+    data = await request.post()
+    iac = ''.join([v.lower() for v in data.values()][:3])
+    try:
+        iac = UUID(iac, version=4)
+    except ValueError:
+        flash(request, "Please provide the unique access code printed on your invitation letter or form.")
+        response = aiohttp_jinja2.render_template("index.html", request, {})
+        return response
+
+    async with request.app.http_session_pool.get('http://ons.gov.uk') as resp:
+        assert resp.status == 200
+
+    return Response(text=iac)
 
 
 @aiohttp_jinja2.template("base.html")

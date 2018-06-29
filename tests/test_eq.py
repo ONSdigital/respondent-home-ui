3a9998439bf9e9b0ae3874416ca7a30c8901dc17
liamtoozer <liam.toozer@gmail.com>
2018-06-29 20:27:55 +0100

    Add date format functional tests

---
+++ tests/test_eq.py
@@ -0,0 +1,31 @@
+import unittest
+
+from app.eq import format_date
+from app.exceptions import InvalidEqPayLoad
+
+
+class TestGenerateEqURL(unittest.TestCase):
+
+    def test_handle_response_raises_client_error_exception(self):
+        pass
+
+    def test_correct_iso8601_date_format(self):
+        # Given a valid date
+        date = '2007-01-25T12:00:00Z'
+
+        # When format_date is called
+        result = format_date(date)
+
+        # Then the date is formatted correctly
+        self.assertEqual(result, '2007-01-25')
+
+    def test_incorrect_date_format(self):
+        # Given an invalid date
+        date = 'invalid_date'
+
+        # When format_date is called
+        with self.assertRaises(InvalidEqPayLoad) as e:
+            format_date(date)
+
+        # Then an InvalidEqPayLoad is raised
+        self.assertEqual(e.exception.message, 'Unable to format invalid_date')

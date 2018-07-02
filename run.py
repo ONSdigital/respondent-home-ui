99ea5473f2c2bce15f70737542432601bd3031b4
Jamie Cox <me@jcox.tech>
2018-07-02 15:55:40 +0100

    Dev script should use dev config

--- run.py
+++ run.py
@@ -3,6 +3,10 @@ from aiohttp import web
 from app.app import create_app
 
 
+if not os.getenv('APP_SETTINGS'):
+    os.environ['APP_SETTINGS'] = 'DevelopmentConfig'
+
+
 if __name__ == '__main__':
     app = create_app()
     web.run_app(app, port=app["PORT"])

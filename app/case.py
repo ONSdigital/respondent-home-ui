6048ecebc7c9624abd30d433eaa315f908a1c5a8
Jamie Cox <me@jcox.tech>
2018-07-02 16:34:03 +0100

    Post case event before launching an eQ

--- app/case.py
+++ app/case.py
@@ -11,3 +11,13 @@ async def get_case(case_id: str, app: Application):
     async with app.http_session_pool.get(f"{app['CASE_URL']}/cases/{case_id}", auth=app["CASE_AUTH"]) as resp:
         resp.raise_for_status()
         return await resp.json()
+
+
+async def post_case_event(case_id: str, category: str, description: str, app: Application):
+    async with app.http_session_pool.post(
+        f"{app.config['CASE_URL']}/cases/{case_id}/events",
+        auth=app["CASE_AUTH"],
+        json={'description': description, 'category': category, 'createdBy': 'RESPONDENT_HOME'}
+    ) as resp:
+        resp.raise_for_status()
+        return await resp.json()

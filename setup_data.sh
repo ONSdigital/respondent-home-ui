474c2f0bd6bcd0a925d640f76ef81d388371612a
Jamie Cox <me@jcox.tech>
2018-07-18 13:13:39 +0100

    Change to use social-test-setup branch of tools

--- setup_data.sh
+++ setup_data.sh
@@ -7,21 +7,13 @@ if [ -d tmp_rm_tools ]; then
     echo "tmp_rm_tools exists - pulling";
     cd tmp_rm_tools; git pull; cd -;
 else
-    git clone --depth 1 ${rm_tools_repo_url} tmp_rm_tools;
+    git clone -b social-test-setup --depth 1 --single-branch ${rm_tools_repo_url} tmp_rm_tools;
 fi;
-collection_exercise_host=${COLLECTION_EXERCISE_SERVICE_HOST:-localhost}
-collection_exercise_port=${COLLECTION_EXERCISE_SERVICE_PORT:-8145}
-collection_exercise_user=${SECURITY_USER_NAME:-admin}
-collection_exercise_password=${SECURITY_USER_PASSWORD:-secret}
-pushd tmp_rm_tools/collex-loader
-
-pipenv run python load.py config/collex-config.json --posturl=http://${collection_exercise_host}:${collection_exercise_port}/collectionexercises \
-    --user=${collection_exercise_user} --password ${collection_exercise_password}
-
-pipenv run python load_events.py config/event-config.json \
-    --posturl=http://${collection_exercise_host}:${collection_exercise_port}/collectionexercises/{id}/events \
-    --geturl=http://${collection_exercise_host}:${collection_exercise_port}/collectionexercises/{exercise_ref}/survey/{survey_ref} \
-    --user=${collection_exercise_user} --password ${collection_exercise_password}
+cp setup.env tmp_rm_tools/social-test-setup/.env
+cp tests/test_data/sample/social-survey-sample.csv tmp_rm_tools/social-test-setup/data/test-1-sample.csv
+pushd tmp_rm_tools/social-test-setup
+pipenv install
+make setup-and-execute
 popd
 rm -rf tmp_rm_tools
 echo "Integration tests can now be run"

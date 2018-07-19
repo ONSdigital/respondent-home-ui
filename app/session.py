4d5b36465bfe822a13e72a00e532bc1a2840a9ac
Jamie Cox <me@jcox.tech>
2018-07-19 15:54:46 +0100

    Ensure session secret key shared between applications as env var

--- app/session.py
+++ app/session.py
@@ -3,9 +3,15 @@ import base64
 from aiohttp_session import session_middleware
 from aiohttp_session.cookie_storage import EncryptedCookieStorage
 from cryptography import fernet
+from envparse import Env
+
+
+def generate_new_key():
+    # NB: should only be used to generate a new environment variable
+    fernet_key = fernet.Fernet.generate_key()
+    return base64.urlsafe_b64decode(fernet_key)
 
 
 def setup():
-    fernet_key = fernet.Fernet.generate_key()
-    secret_key = base64.urlsafe_b64decode(fernet_key)
+    secret_key = Env().str('SECRET_KEY')
     return session_middleware(EncryptedCookieStorage(secret_key))

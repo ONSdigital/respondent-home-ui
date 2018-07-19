49244d68dedaa20ee896f83fd39653be0d385a68
Ben Jefferies <benjefferies@echosoft.uk>
2018-07-19 08:44:34 +0100

    Move ci config to respondent-home-ui

---
+++ ci/smoke_tests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+set -e
+
+cd respondent-home-ui-source
+
+# Install libssl-dev for python cryptography lib
+apt-get install libssl-dev -y
+pipenv install --dev
+pipenv run inv smoke

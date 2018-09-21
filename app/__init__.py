562040003ac3b3883c5f3fb7aaad8607de5ca53c
liamtoozer <liam.toozer@gmail.com>
2018-09-21 13:27:04 +0100

    First pass for clickable flash msgs

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ CONNECTION_ERROR_MSG = "Service connection error. Please try again later."
 REDIRECT_FAILED_MSG = "Failed to redirect to survey. Please try again later."
 SERVER_ERROR_MSG = "Server error. Please try again."
 
-BAD_CODE_MSG = "Please enter the 12 digit household access code provided to you by ONS."
+BAD_CODE_MSG = {'text': 'Please enter the 12 digit household access code provided to you by ONS.', "clickable": False}
 BAD_RESPONSE_MSG = "Bad response from server. Please try again."
 CODE_USED_MSG = "The unique access code entered has already been used."
 INVALID_CODE_MSG = "Invalid access code. Please enter the 12 digit household access code provided to you by ONS."

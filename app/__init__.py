3bf739ee71d9823e2ca859360b8f22c86b77f10d
jrbarnes9 <jrbarnes9@hotmail.com>
2018-09-21 14:29:13 +0100

    Add/remove link in error msg  dependent on error type

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 VERSION = '0.0.1'
 
 
-CONNECTION_ERROR_MSG = "Service connection error. Please try again later."
-REDIRECT_FAILED_MSG = "Failed to redirect to survey. Please try again later."
-SERVER_ERROR_MSG = "Server error. Please try again."
+CONNECTION_ERROR_MSG = {'text': 'Service connection error. Please try again later.', "clickable": False}
+REDIRECT_FAILED_MSG = {'text': 'Failed to redirect to survey. Please try again later.', "clickable": False}
+SERVER_ERROR_MSG = {'text': 'Server error. Please try again.', "clickable": True}
 
-BAD_CODE_MSG = {'text': 'Please enter the 12 digit household access code provided to you by ONS.', "clickable": False}
-BAD_RESPONSE_MSG = "Bad response from server. Please try again."
-CODE_USED_MSG = "The unique access code entered has already been used."
-INVALID_CODE_MSG = "Invalid access code. Please enter the 12 digit household access code provided to you by ONS."
-NOT_AUTHORIZED_MSG = "You are not authorised to access this service."
+BAD_CODE_MSG = {'text': 'Please enter the 12 digit household access code provided to you by ONS.', "clickable": True}
+BAD_RESPONSE_MSG = {'text': 'Bad response from server. Please try again.', "clickable": True}
+CODE_USED_MSG = {'text': 'The unique access code entered has already been used.', "clickable": True}
+INVALID_CODE_MSG = {'text': 'Invalid access code. Please enter the 12 digit household access code provided to you by ONS.', "clickable": True}
+NOT_AUTHORIZED_MSG = {'text': 'You are not authorised to access this service.', "clickable": False}

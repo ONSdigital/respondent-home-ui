58f8edeaaebb0625e455a199867fe8f7bfd688ee
jrbarnes9 <jrbarnes9@hotmail.com>
2018-09-21 10:07:22 +0100

    Updated error messages

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,12 @@
 VERSION = '0.0.1'
 
 
-CONNECTION_ERROR_MSG = "Service connection error"
-REDIRECT_FAILED_MSG = "Failed to redirect to survey"
-SERVER_ERROR_MSG = "Server error"
+CONNECTION_ERROR_MSG = "Service connection error. Please try again later."
+REDIRECT_FAILED_MSG = "Failed to redirect to survey. Please try again later."
+SERVER_ERROR_MSG = "Server error. Please try again."
 
-BAD_CODE_MSG = "Please enter the 12 character unique access code provided to you by ONS."
-BAD_RESPONSE_MSG = "Bad response from server. Please try again"
-CODE_USED_MSG = "The unique access code entered has already been used"
-INVALID_CODE_MSG = "Invalid access code"
-NOT_AUTHORIZED_MSG = "You are not authorized to access this service."
+BAD_CODE_MSG = "Please enter the 12 digit household access code provided to you by ONS."
+BAD_RESPONSE_MSG = "Bad response from server. Please try again."
+CODE_USED_MSG = "The unique access code entered has already been used."
+INVALID_CODE_MSG = "Invalid access code. Please enter the 12 character unique access code provided to you by ONS."
+NOT_AUTHORIZED_MSG = "You are not authorised to access this service."

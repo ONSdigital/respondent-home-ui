a9d1f111302c302a41ee32a7f35ff214bbdf439f
liamtoozer <liam.toozer@gmail.com>
2018-09-28 14:19:33 +0100

    Remove accidental NOQA

--- app/__init__.py
+++ app/__init__.py
@@ -5,7 +5,7 @@ CONNECTION_ERROR_MSG = {'text': 'There was a problem connecting to this study. P
 REDIRECT_FAILED_MSG = {'text': 'There was a problem connecting to this study. Please try again later.', "clickable": False}    # NOQA
 SERVER_ERROR_MSG = {'text': 'There was an error, please enter your access code and try again.', "clickable": True}
 
-BAD_CODE_MSG = {'text': 'Please enter the 12 character access code provided to you by ONS.', "clickable": True}     # NOQA
+BAD_CODE_MSG = {'text': 'Please enter the 12 character access code provided to you by ONS.', "clickable": True}
 BAD_RESPONSE_MSG = {'text': 'There was an error, please enter your access code and try again.', "clickable": True}
 CODE_USED_MSG = {'text': 'Thank you. We have received a response for your address. No further action is required.\n\nIf you need further assistance, please phone us for free on 0800 085 7376.', "clickable": True}    # NOQA
 INVALID_CODE_MSG = {'text': 'Invalid access code. Please check your access code and web address to continue.', "clickable": True}  # NOQA

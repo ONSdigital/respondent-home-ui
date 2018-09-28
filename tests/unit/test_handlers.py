5bc3d712d93f53c09dbde494766dc1cfd8a28848
liamtoozer <liam.toozer@gmail.com>
2018-09-28 14:24:34 +0100

    Update test from earlier change

--- tests/unit/test_handlers.py
+++ tests/unit/test_handlers.py
@@ -417,7 +417,7 @@ class TestHandlers(RHTestCase):
             self.assertLogLine(cm, "Attempt to use an invalid access code", client_ip=None)
 
         self.assertEqual(response.status, 200)
-        self.assertIn(BAD_CODE_MSG.get('text').encode(), await response.content.read())
+        self.assertIn(INVALID_CODE_MSG.get('text').encode(), await response.content.read())
 
     @unittest_run_loop
     async def test_post_index_iac_service_403(self):

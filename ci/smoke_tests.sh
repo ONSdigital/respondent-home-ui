4a29ffc66af3ec17f468348dff6c887a8a8bfcd2
liamtoozer <liam.toozer@gmail.com>
2018-10-08 14:21:01 +0100

    Add pip workaround for pipenv

--- ci/smoke_tests.sh
+++ ci/smoke_tests.sh
@@ -6,5 +6,7 @@ cd respondent-home-ui-source
 # Install libssl-dev for python cryptography lib
 apt-get install libssl-dev -y
 apt-get install python3-dev -y
+pip install --upgrade pip==18.0
+pipenv run pip install --upgrade pip==18.0
 pipenv install --dev
 pipenv run inv smoke

f245111599cd9c490f9a6ec474701ff3467fc016
Jamie Cox <me@jcox.tech>
2018-10-08 10:11:57 +0100

    Fix typo in closed info message

--- app/__init__.py
+++ app/__init__.py
@@ -10,4 +10,4 @@ INVALID_CODE_MSG = {'text': 'Invalid access code. Please check your access code 
 NOT_AUTHORIZED_MSG = {'text': 'There was a problem connecting to this study. Please try again later.', "level": "ERROR"}  # NOQA
 
 CODE_USED_MSG = {'text': 'Thank you. We have received a response for your address. No further action is required.\nIf you need further assistance, please phone us for free on 0800 085 7376.', "level": "INFO"}    # NOQA
-CLOSED_CE_MSG = {'text': 'This study is now closed. Thanks you for your interest in participating.', "level": "INFO"}  # NOQA
+CLOSED_CE_MSG = {'text': 'This study is now closed. Thank you for your interest in participating.', "level": "INFO"}  # NOQA

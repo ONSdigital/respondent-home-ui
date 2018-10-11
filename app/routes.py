e7921749f1d9f3bcddc07dc1f05f53b92961c9a0
liamtoozer <liam.toozer@gmail.com>
2018-10-11 09:40:39 +0100

    Make URL path prefix configurable (#51)
    * Exclude /info from URL prefix
    
    * Refactor handlers to use class-based views
    
    This allows us to group functions that are solely used by a handler as well as defining Flask-like routing with decorators on the classes.
    
    * Make use of utils to add views as resources
    
    This allows us to use the ViewClass:get notation for url lookup as well as improve our tests without manually manipulating the path prefixes.
    These changes also ensure any and all bad path entry redirects back to root, e.g.: /prefix -> /prefix/

--- app/routes.py
+++ app/routes.py
@@ -1,21 +1,11 @@
-from collections import namedtuple
+from aiohttp_utils.routing import add_resource_context
 
-from . import handlers
-
-
-Route = namedtuple("Route", ["method", "path", "handler", "name"])
+from .handlers import routes
 
 
-ROUTES = [
-    Route("GET",  "/", handler=handlers.get_index,  name="get_index"),
-    Route("POST", "/", handler=handlers.post_index, name="post_index"),
-    Route("GET", "/info", handler=handlers.get_info, name="get_info"),
-]
-
-
-def setup(app, url_prefix=""):
-    """Set up routes."""
-    for route in ROUTES:
-        method, url, handler, name = route
-        full_url = f"/{url.lstrip('/')}"
-        app.router.add_route(method, full_url, handler, name=name)
+def setup(app, url_path_prefix):
+    """Set up routes as resources so we can use the `Index:get` notation for URL lookup."""
+    for route in routes:
+        prefix = url_path_prefix if route.kwargs.get('use_prefix', True) else ''
+        with add_resource_context(app, module='app.handlers', url_prefix=prefix) as new_route:
+            new_route(route.path, route.handler())

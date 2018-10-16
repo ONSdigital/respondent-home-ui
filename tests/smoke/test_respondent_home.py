8d32fc30e5e04cd311b788bc2e9cf3cdea70deb4
liamtoozer <liam.toozer@gmail.com>
2018-10-16 14:46:51 +0100

    Update smoke test assertion (#55)
    * Update smoke test assertion

--- tests/smoke/test_respondent_home.py
+++ tests/smoke/test_respondent_home.py
@@ -8,24 +8,29 @@ class TestRespondentHome(unittest.TestCase):
 
     def test_can_access_respondent_home_homepage(self):
         # Given
-        url = os.getenv('RESPONDENT_HOME_URL')
-        if not url:
+        respondent_home_url = os.getenv('RESPONDENT_HOME_URL')
+        if not respondent_home_url:
             self.fail('RESPONDENT_HOME_URL not set')
+
+        url_prefix = os.getenv('URL_PATH_PREFIX', '')
+
+        url = f'{respondent_home_url}{url_prefix}'
 
         # When
         resp = requests.get(url, verify=False)
 
         # Then
-        self.assertEqual(resp.status_code, 200, resp.status_code)
+        self.assertEqual(resp.status_code, 200, url)
 
     def test_can_access_required_services(self):
         # Given
-        url = os.getenv('RESPONDENT_HOME_URL')
+        url = os.getenv('RESPONDENT_HOME_INTERNAL_URL')
         if not url:
-            self.fail('RESPONDENT_HOME_URL not set')
+            self.fail('RESPONDENT_HOME_INTERNAL_URL not set')
 
         # When
         resp = requests.get(f'{url}/info?check=true', verify=False).json()
 
         # Then
         self.assertEqual(resp['ready'], True, resp)
+        self.assertEqual(resp['name'], "respondent-home-ui", resp)

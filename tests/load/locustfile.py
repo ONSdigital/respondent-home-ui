696c7a8456f6cd603eb5bc1cfeb660e8f07f305f
Jamie Cox <me@jcox.tech>
2018-11-15 11:03:21 +0000

    Separate user behaviour

--- tests/load/locustfile.py
+++ tests/load/locustfile.py
@@ -35,15 +35,25 @@ class UserBehavior(TaskSet):
     def contact(self):
         self.client.get('/contact-us')
 
+
+class InvalidUserBehaviour(TaskSet):
+
+    @task(8)
+    def index(self):
+        self.client.get("/")
+
     @task(2)
     def use_invalid_code(self):
-        self.client.post("/", {
+        self.client.post("/?valid=false", {
             'iac1': '1234', 'iac2': '5678', 'iac3': '9012', 'action[save_continue]': '',
         })
 
+
+class PatientUserBehaviour(UserBehavior):
+
     @task(4)
     def launch_survey(self):
-        self.client.post("/", {
+        self.client.post("/?valid=true", {
             'iac1': self.access_code[:4],
             'iac2': self.access_code[4:8],
             'iac3': self.access_code[8:],
@@ -51,11 +61,23 @@ class UserBehavior(TaskSet):
         })
 
 
-class Respondent(HttpLocust):
-    task_set = UserBehavior
+class ImpatientUserBehaviour(UserBehavior):
+
+    @task(4)
+    def launch_survey_impatiently(self):
+        self.client.post("/?valid=true&impatient=15s", {
+            'iac1': self.access_code[:4],
+            'iac2': self.access_code[4:8],
+            'iac3': self.access_code[8:],
+            'action[save_continue]': '',
+        }, timeout=15)
+
+
+class _BaseRespondent(HttpLocust):
+    access_codes = []
     min_wait = 5000
     max_wait = 9000
-    access_codes = []
+    task_set = UserBehavior
 
     @classmethod
     def setup(cls):
@@ -64,3 +86,18 @@ class Respondent(HttpLocust):
             if cls.access_codes:
                 break
             time.sleep(2)
+
+
+class InvalidRespondent(_BaseRespondent):
+    task_set = InvalidUserBehaviour
+    weight = 1
+
+
+class PatientRespondent(_BaseRespondent):
+    task_set = PatientUserBehaviour
+    weight = 2
+
+
+class ImpatientRespondent(_BaseRespondent):
+    task_set = ImpatientUserBehaviour
+    weight = 4

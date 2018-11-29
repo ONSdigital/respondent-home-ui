3d56a83639156b87a4c591e0800929190ebd3e12
Jamie <me@jcox.tech>
2018-11-29 13:07:53 +0000

    Improve help of PM script to show message usage (#90)

--- app/__init__.py
+++ app/__init__.py
@@ -7,4 +7,4 @@ BAD_RESPONSE_MSG = {'text': 'There was an error, please enter your access code a
 INVALID_CODE_MSG = {'text': 'Please re-enter your access code and try again.', "clickable": True, "level": "ERROR", "type": "INVALID_CODE"}  # NOQA
 NOT_AUTHORIZED_MSG = {'text': 'There was a problem connecting to this study. Please try again later.', "level": "ERROR", "type": "SYSTEM_AUTH_ERROR"}  # NOQA
 
-MAINTENANCE_MSG = {"text": "This site will be temporarily unavailable for maintenance <strong>{custom_message}</strong>.\nWe apologise for any inconvenience this may cause.", "level": "INFO", "type": "PLANNED_MAINTENANCE"}  # NOQA
+MAINTENANCE_MSG = {"text": "This site will be temporarily unavailable for maintenance <strong>{message}</strong>.\nWe apologise for any inconvenience this may cause.", "level": "INFO", "type": "PLANNED_MAINTENANCE"}  # NOQA
